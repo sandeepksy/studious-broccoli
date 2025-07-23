daf6cb430d7f2852536b30e981baf891ed2c5b8e
sandeepksy <kumarbaadshah7@gmail.com>
2025-07-23 13:47:38 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,10 @@
+music ={
+    "skyfall":"https://www.youtube.com/watch?v=DeumyOzKqgI&list=RDDeumyOzKqgI&start_radio=1",
+    "Aankhon Aankhon":"https://www.youtube.com/watch?v=-Al-S1uishM&list=RD-Al-S1uishM&start_radio=1",
+    "Wavy":"https://www.youtube.com/watch?v=XTp5jaRU3Ws&list=RDXTp5jaRU3Ws&start_radio=1",
+    "fell for you":"https://www.youtube.com/watch?v=SiKfBBoF51w&list=RDSiKfBBoF51w&start_radio=1",
+    "supreme":"https://www.youtube.com/watch?v=AX1zRInC_TA&list=RDAX1zRInC_TA&start_radio=1",
+    "pal pal":"https://www.youtube.com/watch?v=8of5w7RgcTc&list=RDAX1zRInC_TA&index=22",
+    "i guess":"https://www.youtube.com/watch?v=Qg9LxRHLbAk&list=RDAX1zRInC_TA&index=24"
+
+}
